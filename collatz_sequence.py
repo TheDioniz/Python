ed1d215a143664c36eb828c8f5d30bec5403c158
denisu <denisszczukocki@o2.pl>
2016-07-04 22:25:01 +0200

    small, cosmetic adjustment in collatz_sequence.py program

--- collatz_sequence.py
+++ collatz_sequence.py
@@ -4,12 +4,12 @@ def collatz(number):
     is_odd = number % 2
 
     if is_odd:
-        ret_val = (3 * number) +1
+        ret_val = (3 * number) + 1
     else:
-        ret_val = number // 2
+        ret_val = (number // 2)
     return ret_val
 
-#get number from the user
+#get number from the user  - cast string to int at once
 number = int(input())
 
 #validate the number until it's equal to 1
