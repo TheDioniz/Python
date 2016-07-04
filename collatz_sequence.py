93ee9d9bf1f60161840422a8dc87dca4cf7dd8eb
denisu <denisszczukocki@o2.pl>
2016-07-04 22:22:57 +0200

    added collatz_sequence.py program

---
+++ collatz_sequence.py
@@ -0,0 +1,18 @@
+# the simplest impossible math problem - the Collatz Sequence
+# any number you will pass there, it will finally be decreased to 1
+def collatz(number):
+    is_odd = number % 2
+
+    if is_odd:
+        ret_val = (3 * number) +1
+    else:
+        ret_val = number // 2
+    return ret_val
+
+#get number from the user
+number = int(input())
+
+#validate the number until it's equal to 1
+while number != 1:
+    number = collatz(number)
+    print(number)
